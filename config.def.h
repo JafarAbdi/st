c3ef56881bbba13518a2cba9ee86a5b0c3e9910e
JafarAbdi <cafer.abdi@gmail.com>
2023-03-07 20:04:16 +0000

    Change default font to Jebrains Mono and set border to 0 pixel

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
-static int borderpx = 1;
+static char *font = "Jetbrains Mono:pixelsize=14:antialias=true:autohint=true";
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
